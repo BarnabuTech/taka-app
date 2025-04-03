68bb1f1df125efdc5d256d20090ba25c56ddf8e9
BarnabuTech <barnabutech@gmail.com>
2025-04-03 13:45:07 +0300

    Initial Commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,48 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "tailwindcss/nesting": {}, // Required for CSS nesting support
+    "tailwindcss": {},         // Tailwind v4 processor
+    "autoprefixer": {},
+    "postcss-nesting": {},        // Adds vendor prefixes
+  },
 };
 
 export default config;
+
+
+
+
+
+
+
+
+
+
+// import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
+
+// const config = {
+//   plugins: [
+//     tailwindcss,
+//     autoprefixer
+//   ],
+// };
+
+// export default config;
+
+
+
+
+
+
+
+
+
+
+// My initial codes for attempt to use tailwindcss with postcss.
+
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
